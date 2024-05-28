55cd6948fb0fc2d4e0d26fe2f21b3e9d2771c634
Biryulya Mikhail <misaib@mail.ru>
2024-05-29 03:32:05 +0700

    Update main.cpp

--- lab5/main.cpp
+++ lab5/main.cpp
@@ -1,4 +1,4 @@
-#include <math.h>
+#include <cmath>
 #include <mpi.h>
 #include <pthread.h>
 #include <vector>
@@ -6,9 +6,9 @@
 #define LISTS_COUNT 10
 #define TASK_COUNT 1500
 #define SOLVER_FINISHED_WORK (-1)
-#define SENDING_TASKS 656
-#define SENDING_TASK_COUNT 787
-#define NO_TASKS_TO_SHARE (-565)
+#define NO_TASKS_TO_SHARE (-2)
+#define SENDING_TASKS 1111
+#define SENDING_TASK_COUNT 2222
 
 struct threadArgs{
     int rank{};
@@ -38,9 +38,7 @@ void doTask(threadArgs* tArgs) {
     }
     tArgs->remainingTasks = 0;
 }
-/**
- * Эта функция многократно выполняет задачи, синхронизирует процессы, измеряет время выполнения и дисбаланс, а также динамически распределяет задачи между процессами для обеспечения более равномерной загрузки.
- */
+
 void* solver(void* args) {
     auto* tArgs = (threadArgs *)args;
     int rank = tArgs->rank;
@@ -54,16 +52,12 @@ void* solver(void* args) {
         initTasks(tArgs->tasks, TASK_COUNT, i, rank, size);
         doTask(tArgs);
         int response;
-        // printf("Yes %d\n", procRank);
-        //Обмен задачами с другими процессами:
         for (int procIdx = 0; procIdx < size; procIdx++) {
-            //чтобы не обмениваться с самим собой
             if (procIdx != rank) {
                 MPI_Send(&rank, 1, MPI_INT,
                          procIdx, 888, MPI_COMM_WORLD);
                 MPI_Recv(&response, 1, MPI_INT,
                          procIdx, SENDING_TASK_COUNT,MPI_COMM_WORLD, MPI_STATUS_IGNORE);
-                //если reciever что то отправил, то получаем
                 if (response != NO_TASKS_TO_SHARE) {
                     //получает задачи от procIdx
                     MPI_Recv(&tArgs->tasks[0], response, MPI_INT,
@@ -75,12 +69,9 @@ void* solver(void* args) {
                 }
             }
         }
-        //Замер времени выполнения и вычисление дисбаланса:
         finishTime = MPI_Wtime();
         iterationDuration = finishTime - startTime;
-        //находит максимальную длительность итерации среди всех процессов
         MPI_Allreduce(&iterationDuration, &longest, 1, MPI_DOUBLE, MPI_MAX,MPI_COMM_WORLD);
-        //находит минимальную длительность среди всех процессов
         MPI_Allreduce(&iterationDuration, &shortest, 1, MPI_DOUBLE, MPI_MIN,MPI_COMM_WORLD);
         printf("iteration do %f %f in time %f\n", longest, shortest, iterationDuration);
         tArgs->summaryDisbalance += (longest - shortest) / longest;
@@ -91,9 +82,7 @@ void* solver(void* args) {
     }
     return nullptr;
 }
-/**
- * Функция предназначена для обработки запросов от других процессов на получение задач. Она безопасно делит оставшиеся задачи между процессами, используя мьютексы для синхронизации доступа к общим данным, и использует MPI для обмена сообщениями между процессами.
- */
+
 void* reciever(void* args) {
     auto* tArgs = (threadArgs *)args;
     int rank = tArgs->rank;
@@ -106,20 +95,14 @@ void* reciever(void* args) {
         }
         rankSentRequest = request;
         pthread_mutex_lock(&tArgs->mutex);
-        // Проверка наличия задач для распределения
         if (tArgs->remainingTasks >= 2) {
-            // Расчет количества задач для передачи
             answer = tArgs->remainingTasks / (rank + 1);
             tArgs->remainingTasks = tArgs->remainingTasks / (rank + 1);
             printf("sharing %d from %d to %d\n", answer, rank, rankSentRequest);
-            // Отправка информации о количестве задач, которые будут переданы
-            MPI_Send(&answer, 1, MPI_INT, rankSentRequest, SENDING_TASK_COUNT,
-                     MPI_COMM_WORLD);
-            // Отправка самих задач
+            MPI_Send(&answer, 1, MPI_INT, rankSentRequest, SENDING_TASK_COUNT, MPI_COMM_WORLD);
             MPI_Send(&tArgs->tasks[TASK_COUNT - answer], answer, MPI_INT,
                      rankSentRequest, SENDING_TASKS, MPI_COMM_WORLD);
         } else {
-            // Если задач недостаточно для передачи
             answer = NO_TASKS_TO_SHARE;
             MPI_Send(&answer, 1, MPI_INT, rankSentRequest, SENDING_TASK_COUNT, MPI_COMM_WORLD);
         }
@@ -138,10 +121,8 @@ int main() {
     pthread_create(&args.threads[0], nullptr, solver, &args);
     if (args.size != 1) {
         pthread_create(&args.threads[1], nullptr, reciever, &args);
-        //ожидание завершения порожденных потоков
         pthread_join(args.threads[1], nullptr);
     }
-    //ожидание завершения порожденных потоков
     pthread_join(args.threads[0], nullptr);
     double finish = MPI_Wtime() - start;
     double time;
